0dd670fddc269959263d50685ffa351a77f43a5d
iStrafeNubzHDyt <pixelgameskontakt@gmail.com>
2018-04-28 16:07:30 +0200

    Update PixelgamesConsoleManager.php

--- src/Author/PixelgamesConsoleManager/PixelgamesConsoleManager.php
+++ src/Author/PixelgamesConsoleManager/PixelgamesConsoleManager.php
@@ -107,8 +107,8 @@ class PixelgamesConsoleManager extends PluginBase {
                     $sender->sendMessage("§e-------------------------------------");
                     $sender->sendMessage("§ePlugin von iStrafeNubzHDyt");
                     $sender->sendMessage("§bName: PixelgamesConsoleManager");
-                    $sender->sendMessage("§bVersion: 0.3");
-                    $sender->sendMessage("§bFür PocketMine-API: 3.0.0-ALPHA11");
+                    $sender->sendMessage("§bVersion: 0.4");
+                    $sender->sendMessage("§bFür PocketMine-API: 3.0.0-ALPHA12");
                     $sender->sendMessage("§6Permissions: pgcm.command");
                     $sender->sendMessage("§eSpeziell für PIXELGAMES entwickelt");
                     $sender->sendMessage("§e-------------------------------------");
